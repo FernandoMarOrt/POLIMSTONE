e11f1e82a7047a8eb97a977b78c10be3f7fe1c89
FernandoMarOrt <fernandomartinezortiz003@gmail.com>
2024-06-16 21:09:27 +0200

    FIN DE LA PAGINA WEB , FALTA MODIFICAR EL PHP PARA EMAIL Y ALGUNOS PRODUCTOS

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,15 @@
 $(document).ready(function () {
 
-      $("#lightSlider").lightSlider({
-        item:1,
-        loop:true,
-    }); 
 
+
+
+    let clickCubremuro = "div#contenido>main>div#productos>div#cubremurosD";
+    let clickCubrepilares = "div#contenido>main>div#productos>div#cubrepilaresD";
+    let clickJambas = "div#contenido>main>div#productos>div#jambasD";
+    let clickUmbrales = "div#contenido>main>div#productos>div#umbralesD"; 
+    let clickViertaguas = "div#contenido>main>div#productos>div#viertaguasD";
+    let clickFrenteforjado = "div#contenido>main>div#productos>div#frenteforjadoD";
+    let clickRercercos = "div#contenido>main>div#productos>div#recercosD";
 
     //Animacion del menu version movil
     let menuClick = "#menuclick";
@@ -34,39 +39,13 @@ $(document).ready(function () {
 
     $(".menuProductos").on({
         click: function () {
-            window.location.href = "./paginas/productos.html";
+            window.location.href =  "./index.html#productos";
         }
     })
 
     $(".menuContactos").on({
         click: function () {
             window.location.href = "./index.html#contacto";
-        }
-    })
-
-    //Pagina productos
-
-    $(".menuInicio2").on({
-        click: function () {
-            window.location.href = "../index.html";
-        }
-    })
-
-    $(".menuNosotros2").on({
-        click: function () {
-            window.location.href = "../index.html#nosotros";
-        }
-    })
-
-    $(".menuProductos2").on({
-        click: function () {
-            window.location.href = "productos.html";
-        }
-    })
-
-    $(".menuContactos2").on({
-        click: function () {
-            window.location.href = "../index.html#contacto";
         }
     })
 
@@ -93,5 +72,143 @@ $(document).ready(function () {
     );
 
 
+    $(clickCubremuro).on({
+        click: function () {
+
+            $("#cubremuros").fadeOut();
+
+            if ($("#cubremuros").css("display") === "none") {
+                $("#cubremuros").fadeIn();
+                $("#flechaCubre").css({ transform: 'rotate(180deg)', transition: 'transform 0.5s ease' });
+            } else {
+                $("#flechaCubre").css({ transform: 'rotate(0deg)', transition: 'transform 0.5s ease' });
+                
+            }
+        }
+
+
+    });
+
+
+    $(clickCubrepilares).on({
+        click: function () {
+
+            $("#cubrepilares").fadeOut();
+
+            if ($("#cubrepilares").css("display") === "none") {
+                $("#cubrepilares").fadeIn();
+                $("#flechaCubre2").css({ transform: 'rotate(180deg)', transition: 'transform 0.5s ease' });
+            } else {
+                $("#flechaCubre2").css({ transform: 'rotate(0deg)', transition: 'transform 0.5s ease' });
+            }
+        }
+
+
+    });
+
+
+    $(clickJambas).on({
+        click: function () {
+
+            $("#jambas").fadeOut();
+
+            if ($("#jambas").css("display") === "none") {
+                $("#jambas").fadeIn();
+                $("#flechaJambas").css({ transform: 'rotate(180deg)', transition: 'transform 0.5s ease' });
+            } else {
+                $("#flechaJambas").css({ transform: 'rotate(0deg)', transition: 'transform 0.5s ease' });
+            }
+        }
+
+
+    });
+
+
+
+
+    $(clickUmbrales).on({
+        click: function () {
+
+            $("#umbrales").fadeOut();
+
+            if ($("#umbrales").css("display") === "none") {
+                $("#umbrales").fadeIn();
+                $("#flechaUmbrales").css({ transform: 'rotate(180deg)', transition: 'transform 0.5s ease' });
+            } else {
+                $("#flechaUmbrales").css({ transform: 'rotate(0deg)', transition: 'transform 0.5s ease' });
+            }
+        }
+
+
+    });
+
+
+
+    $(clickViertaguas).on({
+        click: function () {
+
+            $("#viertaguas").fadeOut();
+
+            if ($("#viertaguas").css("display") === "none") {
+                $("#viertaguas").fadeIn();
+                $("#flechaViertaguas").css({ transform: 'rotate(180deg)', transition: 'transform 0.5s ease' });
+            } else {
+                $("#flechaViertaguas").css({ transform: 'rotate(0deg)', transition: 'transform 0.5s ease' });
+            }
+        }
+
+
+    });
+
+
+
+
+    $(clickFrenteforjado).on({
+        click: function () {
+
+            $("#frenteforjado").fadeOut();
+
+            if ($("#frenteforjado").css("display") === "none") {
+                $("#frenteforjado").fadeIn();
+                $("#flechaForjado").css({ transform: 'rotate(180deg)', transition: 'transform 0.5s ease' });
+            } else {
+                $("#flechaForjado").css({ transform: 'rotate(0deg)', transition: 'transform 0.5s ease' });
+            }
+        }
+
+
+    });
+
+
+    
+
+    $(clickRercercos).on({
+        click: function () {
+
+            $("#recercos").fadeOut();
+
+            if ($("#recercos").css("display") === "none") {
+                $("#recercos").fadeIn();
+                $("#flechaRecercos").css({ transform: 'rotate(180deg)', transition: 'transform 0.5s ease' });
+            } else {
+                $("#flechaRecercos").css({ transform: 'rotate(0deg)', transition: 'transform 0.5s ease' });
+            }
+        }
+
+
+    });
+
+   
+    $("#cubrepilares").hide();
+    $("#jambas").hide();
+    $("#umbrales").hide();
+    $("#viertaguas").hide();
+    $("#frenteforjado").hide();
+    $("#recercos").hide();
+
+
+
 
 })
+
+
